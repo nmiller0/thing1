e8daf7809ddf734cdf6d8320119e9f3ee6bfca9c
Neal Miller <neal.miller@outlook.com>
2019-02-04 10:49:30 -0500

    Added title text

--- startScreen.py
+++ startScreen.py
@@ -15,6 +15,15 @@ def startButton(x,y):
 black = (0,0,0)
 white = (255,255,255)
 
+def textObjects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def titleDisplay(text):
+    largeText = pygame.font.Font('freesansbold.ttf',32)
+    TextSurf, TextRect = textObjects(text, largeText)
+    TextRect.center = ((display_width/2),(display_height/3))
+    gameDisplay.blit(TextSurf, TextRect)
 
 x =  (display_width * 0.40)
 y = (display_height * 0.8)
@@ -28,7 +37,9 @@ while not crashed:
             crashed = True
 
         print(event)
+
     gameDisplay.fill(white)
+    titleDisplay("thing1: futuristic voice chess from the future")
     startButton(x,y)
     pygame.display.update()
     clock.tick(60)
