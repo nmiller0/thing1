c6cb7d1537e6ae6212594114107a8fdcca3ba5e6
LattyWasTaken <alecsteele96@gmail.com>
2019-02-04 11:45:03 -0500

    Add files via upload
    Graphics test

---
+++ test.py
@@ -0,0 +1,45 @@
+import os,sys
+import pygame as pg
+
+bg = pg.image.load(os.path.join("images", "Board.png"))
+
+class Character:
+    def __init__(self,rect):
+        self.rect = pg.Rect(rect)
+        self.click = False
+        self.image = pg.Surface(self.rect.size).convert()
+        self.image.fill((255,0,0))
+    def update(self,surface):
+        if self.click:
+            self.rect.center = pg.mouse.get_pos()
+        surface.blit(self.image,self.rect)
+
+def main(Surface,Player):
+    game_event_loop(Player)
+    Surface.fill(0)
+    Player.update(Surface)
+
+
+def game_event_loop(Player):
+    for event in pg.event.get():
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if Player.rect.collidepoint(event.pos):
+                Player.click = True
+        elif event.type == pg.MOUSEBUTTONUP:
+            Player.click = False
+        elif event.type == pg.QUIT:
+            pg.quit(); sys.exit()
+
+if __name__ == "__main__":
+    os.environ['SDL_VIDEO_CENTERED'] = '1'
+    pg.init()
+    Screen = pg.display.set_mode((900,900))
+    MyClock = pg.time.Clock()
+    MyPlayer = Character((0,0,150,150))
+    Screen.blit(bg, (0,0))
+    pg.display.update()
+    MyPlayer.rect.center = Screen.get_rect().center   
+    while 1:
+        main(Screen,MyPlayer)
+        pg.display.update()
+        MyClock.tick(60)
