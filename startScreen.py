bacfdb17a72a6f4fec8940ad11200ab26dc22030
Neal Miller <neal.miller@outlook.com>
2019-02-11 12:10:43 -0500

    Buttons now handle clicks

--- startScreen.py
+++ startScreen.py
@@ -1,21 +1,16 @@
 import pygame
 
 pygame.init()
-display_width = 800
-display_height = 600
-
+display_width = 900
+display_height = 900
+button_width = 200
+button_height = 100
 gameDisplay = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('thing1: voice chess')
-imgStartButton = pygame.image.load('./assets/ui/blue_button00.png')
+clock = pygame.time.Clock()        
 
-clock = pygame.time.Clock()
-def startButton(x,y):
-    gameDisplay.blit(imgStartButton,(x,y))
-    largeText = pygame.font.Font('freesansbold.ttf',32)
-    TextSurf, TextRect = textObjects("Start", largeText)
-    TextRect.center = (x+(display_width/9),y+(display_height/20))
-    gameDisplay.blit(TextSurf, TextRect)          
-
+blue = (66, 134, 244)
+green = (25, 122, 46)
 black = (0,0,0)
 white = (255,255,255)
 
@@ -31,21 +26,37 @@ def titleDisplay(text):
 
 
 x =  (display_width * 0.40)
-y = (display_height * 0.8)
+y = (display_height * 0.6)
+
+def button(msg,x,y,w,h,ic,ac,action=None):
+    mouse = pygame.mouse.get_pos()
+    click = pygame.mouse.get_pressed()
+    print(click)
+    if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        pygame.draw.rect(gameDisplay, ac,(x,y,w,h))
+        if click[0] == 1 and action != None:
+            action()         
+    else:
+        pygame.draw.rect(gameDisplay, ic,(x,y,w,h))
+
+    smallText = pygame.font.SysFont("freesansbold.ttf",24)
+    textSurf, textRect = textObjects(msg, smallText)
+    textRect.center = ( (x+(w/2)), (y+(h/2)) )
+    gameDisplay.blit(textSurf, textRect)
 
 crashed = False
 
 while not crashed:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-
         print(event)
 
     gameDisplay.fill(white)
     titleDisplay("thing1: futuristic voice chess from the future")
-    startButton(x,y)
+    button("Start", x,y,button_width,button_height,blue,green)
+    button("Quit", x,y+150,button_width,button_height,blue,green, exit)
+
     pygame.display.update()
     clock.tick(60)
 
