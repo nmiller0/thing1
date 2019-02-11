704c5e76e82053016c393417364bc3ca5dba4848
Neal Miller <neal.miller@outlook.com>
2019-02-11 12:31:13 -0500

    Added submenu for single player

--- startScreen.py
+++ startScreen.py
@@ -26,7 +26,7 @@ def titleDisplay(text):
 
 
 x =  (display_width * 0.40)
-y = (display_height * 0.6)
+y = (display_height * 0.5)
 
 def button(msg,x,y,w,h,ic,ac,action=None):
     mouse = pygame.mouse.get_pos()
@@ -44,19 +44,34 @@ def button(msg,x,y,w,h,ic,ac,action=None):
     textRect.center = ( (x+(w/2)), (y+(h/2)) )
     gameDisplay.blit(textSurf, textRect)
 
-crashed = False
+def singlePlayerMenu():
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+            print(event)
+        gameDisplay.fill(white)
+        titleDisplay("Select which color you'd like to play")
+        button("Start as Black", x,y,button_width,button_height,blue,green)
+        button("Start as White", x,y+150,button_width,button_height,blue,green)
+        button("Back", x,y+300,button_width,button_height,blue,green, __main__)
+        pygame.display.update()
+        clock.tick(60)
 
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-        print(event)
+def __main__():
+    crashed = False
+    while not crashed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                crashed = True
+            print(event)
 
-    gameDisplay.fill(white)
-    titleDisplay("thing1: futuristic voice chess from the future")
-    button("Start", x,y,button_width,button_height,blue,green)
-    button("Quit", x,y+150,button_width,button_height,blue,green, exit)
+        gameDisplay.fill(white)
+        titleDisplay("thing1: futuristic voice chess from the future")
+        button("Play Single Player", x,y,button_width,button_height,blue,green, singlePlayerMenu)
+        button("Quit", x,y+150,button_width,button_height,blue,green, exit)
+        pygame.display.update()
+        clock.tick(60)
 
-    pygame.display.update()
-    clock.tick(60)
-
+__main__();
