3d3f6eb0750ab67cc9d45b7eaf572645196393f6
Neal Miller <neal.miller@outlook.com>
2019-02-27 20:35:12 -0500

    Added basic grid snapping

--- test.py
+++ test.py
@@ -1,14 +1,49 @@
 import os,sys
 import pygame as pg
+import math as math
 
 bg = pg.image.load(os.path.join("images", "Board.png"))
-pawn = pg.image.load(os.path.join("images", "pawn.png"))
-pawn = pg.transform.scale(pawn,(100,100))
+#pawn = pg.image.load(os.path.join("images", "Chess_plt60.png"))
+#pawn = pg.transform.scale(pawn,(100,100))
+
+board = []
+startx = 42.5
+starty = 770 #(900-42.5)
+
+for x in range(8):
+    row = []
+    for i in range(8):
+        row.append((startx , starty))
+        startx += 102.5
+    board.append(row)
+    startx = 42.5
+    starty -= 102.5
+
+class piece:
+    def __init__(self, image, scale):
+        self.pos = (0,0)
+        self.image = pg.image.load(os.path.join("images", "Chess_plt60.png"))
+        self.image = pg.transform.scale(self.image,(scale,scale))
+
+def findClosestSquare(pos,board):
+    closestPoint = (0,0)
+    closestPointDist = 900
+    for row in board:
+        for r in row:
+            rDist = distance(pos,(r[0]+50,r[1]+50))
+            if(rDist < closestPointDist):
+                closestPoint = r
+                closestPointDist = rDist
+    return closestPoint
+            
+def distance(p,q):
+    return math.sqrt((p[0]-q[0])**2 + (p[1]-q[1])**2)
 
 x = 0
 y = 0
 r = 0
 
+pawn = piece("Chess_plt60.png",100)
 if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
@@ -22,7 +57,8 @@ if __name__ == "__main__":
                 sys.exit()
             elif event.type == pg.MOUSEBUTTONDOWN:
                 mx,my = pg.mouse.get_pos()
-        Screen.blit(bg, (0,0))        
-        Screen.blit(pawn,(mx-50,my-50))
+                pawn.pos = findClosestSquare((mx,my),board)
+        Screen.blit(bg, (0,0))    
+        Screen.blit(pawn.image,pawn.pos)   
         pg.display.update()
         MyClock.tick(60)
