ee66cb9fea39b21d6b28fc92f00a2d1bdcc70a2a
Neal Miller <neal.miller@outlook.com>
2019-04-17 09:06:47 -0400

    Added move highlighting

--- test.py
+++ test.py
@@ -1,54 +1,62 @@
-import os,sys
+import os
+import sys
 import pygame as pg
 import math as math
 import piece as piece
 import board as board
 import threading
 import queue
-
 import speech_recognition as sr
+import chess
+
+pcBoard = chess.Board()
 
 def listen(x):
-	r = sr.Recognizer()
-	with sr.Microphone() as source:
-		r.pause_threshold = x
-		r.non_speaking_duration = x
-		r.adjust_for_ambient_noise(source, duration = x)
-		audio = r.listen(source)
-	try:
-		command = r.recognize_google(audio)
-	#Loops until something is understood
-	except sr.UnknownValueError:
-		command = listen(x)
-	return command
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = x
+        r.non_speaking_duration = x
+        r.adjust_for_ambient_noise(source, duration=x)
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+    # Loops until something is understood
+    except sr.UnknownValueError:
+        command = listen(x)
+    return command
+
 
 commandQueue = queue.Queue()
 
+
 def get_Command():
-        while True:
-                print("Say 'chess.'")
-                keycheck = listen(0.1)
-                keyphrase = keycheck.split()
-                #change test word here 
-                if "chess" in keyphrase:
-                        print("Say a move.")
-                        command = listen(.5)
-                        print("I heard: ", command)
-                        print("Is that correct?")
-                        keycheck1 = listen(0.5)
-                        keyphrase1 = keycheck1.split()
-                        if "yes" in keyphrase1:
-                                print("Move has been made!")
-                                commandQueue.put(command)
-                                return command
-                                break
+    while True:
+        print("Say 'chess.'")
+        keycheck = listen(0.1)
+        keyphrase = keycheck.split()
+        # change test word here
+        if "chess" in keyphrase:
+            print("Say a move.")
+            command = listen(.5)
+            print("I heard: ", command)
+            print("Is that correct?")
+            keycheck1 = listen(0.5)
+            keyphrase1 = keycheck1.split()
+            if "yes" in keyphrase1:
+                print("Move has been made!")
+                commandQueue.put(command)
+                return command
+                break
+
+
 def parseCommand(command):
     splitCommand = command.lower().split()
     return splitCommand
 
+
 def main():
-        command = get_Command()
-        parseCommand(command)
+    command = get_Command()
+    parseCommand(command)
 
 
 bg = pg.image.load(os.path.join("images", "Board.png"))
@@ -57,49 +65,72 @@ gameBoard = board.board()
 
 pawn = pg.image.load(os.path.join("images", "pawn.png"))
 vt = pg.image.load(os.path.join("images", "test.png"))
-pawn = pg.transform.scale(pawn,(100,100))
+pawn = pg.transform.scale(pawn, (100, 100))
 
 x = 0
 y = 0
 r = 0
 
-pawn = piece.piece("pawn","white")
+pawn = piece.piece("pawn", "white")
 if __name__ == "__main__":
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
-    Screen = pg.display.set_mode((900,900))
+    Screen = pg.display.set_mode((900, 900))
     MyClock = pg.time.Clock()
     pg.display.update()
-    mx,my = pg.mouse.get_pos()
+    mx, my = pg.mouse.get_pos()
     piece = 0
     mouseDown = False
     voiceThread = threading.Thread(target=get_Command)
     voiceThread.start()
     while 1:
+        #check if voice input is over
         if not voiceThread.is_alive():
             voiceThread.join()
             foundCommand = parseCommand(commandQueue.get())
-            print("COMMAND RETURNED: " + foundCommand[0]+ " " + foundCommand[1])
+            print("COMMAND RETURNED: " +
+                  foundCommand[0] + " " + foundCommand[1])
             gameBoard.movePiece(foundCommand[0], foundCommand[2])
             voiceThread = threading.Thread(target=get_Command)
             voiceThread.start()
-        mx,my = pg.mouse.get_pos()
+        mx, my = pg.mouse.get_pos()
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sys.exit()
             elif event.type == pg.MOUSEBUTTONDOWN:
                 mouseDown = True
-                sqClicked = gameBoard.findClosestSquare((mx,my))
+                sqClicked = gameBoard.findClosestSquare((mx, my))
                 piece = gameBoard.findPieceAt(sqClicked)
+                orgCoord = gameBoard.convertPositionToSquare(sqClicked)
             elif event.type == pg.MOUSEBUTTONUP:
                 mouseDown = False
-                piece.pos = gameBoard.findClosestSquare((mx,my))
+                piece.pos = gameBoard.findClosestSquare((mx, my))
+        validMoves = []
 
+        #if the mouse button is down, update piece position (drag)
+        #then iterate to find all valid moves for that square
         if mouseDown:
-            piece.pos = (mx-50,my-50)
-
-        Screen.blit(bg, (0,0))  
-        for p in gameBoard.pieces:  
-            Screen.blit(p.image,p.pos)   
+            piece.pos = (mx-50, my-50)
+            for i in range(8):
+                for x in range(8):
+                    if chess.Move.from_uci(orgCoord+board.oDict[i]+str(x+1)) in pcBoard.legal_moves:
+                        validMoves.append((orgCoord+board.oDict[i]+str(x+1)))
+            
+        #draw the background
+        Screen.blit(bg, (0, 0))
+        
+        #draw green squares on all valid move spots
+        for sq in validMoves:
+                print(str(sq[2]+sq[3]))
+                x = gameBoard.convertSquareToPos(str(sq[2]+sq[3])) 
+                left= x[0]
+                top= x[1]
+                width=95
+                height=95
+                pg.draw.rect(Screen, [0, 255, 0], [left, top, width, height], 0)
+        
+        #draw all game pieces
+        for p in gameBoard.pieces:
+            Screen.blit(p.image, p.pos)
         pg.display.update()
         MyClock.tick(60)
